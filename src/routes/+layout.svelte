34b005aca3e1119b3414ebd9eaf5c1fd8d6a61b8
Justin Davies <therealjustindude@gmail.com>
2023-01-15 13:01:36 -0500

    logo component, intial navigation setup

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<script>
+	import Logo from './components/Logo.svelte'
+</script>
+
+<nav>
+	<Logo />
+  <a href="/resume">Resume</a>
+  <a href="/contact">Contact</a>
+</nav>
+
+<slot></slot>
+
+<style>
+	nav {
+		padding: 8px;
+		display: flex;
+    justify-content: space-around;
+    align-items: center;
+		border-bottom: solid #80808012;
+	}
+</style>
---
+++ src/routes/components/Logo.svelte
@@ -0,0 +1,115 @@
+<a href="/" id="logo_container">
+	<div id="logo_justin">Justin</div>
+	<div id="logo_dv">
+		D<div id="logo_a">a</div>v<div id="logo_ies">ies</div>
+	</div>
+	<div id="logo_e">e</div>
+	
+	
+</a>
+
+<style>
+	@keyframes slideDown {
+		0% {
+			display: block;
+			transform: translateY(0);
+		}
+		100% {
+			transform: translateY(100%);
+			opacity: 0;
+			display: none;
+		}
+	}
+
+	@keyframes slideUp {
+		0% {
+			transform: translateY(-1000%);
+			opacity: 0;
+			display: none;
+		}
+		100% {
+			transform: translateY(0);
+			opacity: 1;
+		}
+	}
+
+	@keyframes slideInFromRight {
+		0% {
+			transform: translateX(1000%);
+			opacity: 0;
+			display: none;
+		}
+		100% {
+			transform: translateX(0);
+			opacity: 1;
+		}
+	}
+
+	@keyframes slideInFromLeft {
+		0% {
+			transform: translateX(-100%);
+			opacity: 0;
+			display: none;
+		}
+		100% {
+			transform: translateX(0);
+			opacity: 1;
+			display: block;
+		}
+	}
+
+	#logo_container {
+		display: flex;
+		justify-content: center;
+    align-items: center;
+    flex-direction: row;
+		font-family: 'Caveat', cursive;
+		font-size: xx-large;
+		text-decoration: none;
+		/* add var */
+    color: black;
+	}
+
+	#logo_dv,
+	#logo_e,
+	#logo_a,
+	#logo_ies,
+	#logo_justin {
+		transition: all 1s ease;
+	}
+
+	#logo_dv {
+		display: flex;
+		opacity: 1;
+	}
+	#logo_e {
+		position: relative;
+		left: -50px;
+		animation-delay: 2.5s;
+		animation-timing-function: ease-out;
+		animation-duration: 0.5s;
+		animation-name: slideDown;
+		animation-fill-mode: forwards;
+	}
+	#logo_a {
+		opacity: 0;
+		animation-delay: 2s;
+		animation-timing-function: ease-in-out;
+		animation-duration: 1s;
+		animation-name: slideUp;
+		animation-fill-mode: forwards;
+	}
+	#logo_ies {
+		animation-delay: 0s;
+		animation-timing-function: ease-in-out;
+		animation-duration: 2s;
+		animation-name: slideInFromRight;
+	}
+	#logo_justin {
+		animation-delay: 0s;
+		animation-timing-function: ease-in-out;
+		animation-duration: 2s;
+		animation-name: slideInFromLeft;
+		margin-right: 4px;
+	}
+</style>

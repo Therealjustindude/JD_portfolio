f8a549bb2fc76adb433234ea7dff09f9ed14bd92
Justin Davies <therealjustindude@gmail.com>
2024-12-31 18:42:02 -0500

    navigation redesigned

--- src/components/NavigationBar.svelte
+++ src/components/NavigationBar.svelte
@@ -39,16 +39,18 @@
     justify-content: space-between;
     align-items: center;
 		background-color: var(--theme-palette-common-background);
-		padding: 8px 16px;
+		border-bottom: 1px solid var(--theme-palette-primary);
+		padding: 24px 0px 8px 0px;
+		margin: 0px 32px 0px 32px;
 		left: 0;
 		right: 0;
-		box-shadow: 0px -16px 24px black;
 		z-index: 1;
 	}
 
 	#link-container {
 		display: flex;
     align-items: center;
+		gap: 8px;
 	}
 
 	/* #contact-link {
@@ -62,25 +64,27 @@
 	a {
 		display: flex;
 		font-family: 'Press Start 2P', cursive;
-		font-size: 6px;
+		font-size: 8px;
 		text-decoration: none;	
 		color: var(--theme-palette-primary);
-		filter: drop-shadow(0px 1px 0.5px var(--theme-palette-secondary));
+		filter: drop-shadow(0.5px 0px 0px var(--theme-palette-secondary));
 		transition: 0.3s;
-		margin: 0px 8px;
 	}
 	a:hover {
 		color: var(--theme-palette-accent);
-		font-size: 7px;
+		font-size: 10px;
 	}
 
 	/* When the browser is below 430px */
 	@media screen and (max-width: 430px) {
+		#nav-container {
+			margin: 0px 16px 0px 16px;
+		}
 		a {
-			font-size: 5px;
+			font-size: 6px;
 		}
 		a:hover {
-			font-size: 6px;
+			font-size: 7px;
 		}
 		/* #contact-link {
 			display: flex;
@@ -95,14 +99,6 @@
 		} */
 		#socials-link {
 			display: flex;
-			font-family: 'Press Start 2P', cursive;
-			text-decoration: none;	
-			color: var(--theme-palette-primary);
-			filter: drop-shadow(0px 1px 0.5px var(--theme-palette-secondary));
-			transition: 0.3s;
-		}
-		#socials-link:hover {
-			color: var(--theme-palette-accent);
 		}
 	}
 	/* When the browser is below 325px */
@@ -110,6 +106,7 @@
 		#nav-container {
 			justify-content: center;
 			flex-wrap: wrap;
+			gap: 8px;
 		}
 	}
 
